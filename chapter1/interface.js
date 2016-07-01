eb5bf96887621c114ad06fa203bd6a022992fb80
Shi Chen <shixchen@deloitte.ca>
2016-07-01 12:28:45 -0400

    add implementation

--- chapter1/interface.js
+++ chapter1/interface.js
@@ -2,7 +2,14 @@
  *  Inserts "doc" into the collection "movies".
  */
 exports.insert = function(db, doc, callback) {
-  // TODO: implement
+  var collection = db.collection('movies');
+  collection.insert(doc, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Inserted Successfully');
+    }
+  });
   callback(null);
 };
 
@@ -13,6 +20,16 @@ exports.insert = function(db, doc, callback) {
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
 exports.byDirector = function(db, director, callback) {
-  // TODO: implement
-  callback(null, []);
+  var collection = db.collection('movies');
+  collection.find({ 'director': director }).sort({'title':1}).toArray(function(err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Found Results");
+      results.forEach(function(doc) {
+        console.log(JSON.stringify(doc))
+      })
+    }
+    callback(null, results);
+  });
 };
